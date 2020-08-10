11acf36fbac592abf3073d61cefcb50be33305e0
JulianaFBarreto <69052505+JulianaFBarreto@users.noreply.github.com>
2020-08-10 17:22:59 -0300

    atualizacao index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://turma6b-julianafb.herokuapp.com/"
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
